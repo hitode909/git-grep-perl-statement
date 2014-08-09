1c2bd252efbe45c046a32514c580f71a31295402
hitode909 <hitode909@gmail.com>
2014-08-09 18:05:55 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a521387..4763651 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension App-GitGrepPerlStatement
    
     {{$NEXT}}
    
    +0.02 2014-08-09T09:05:52Z
    +    - add document
    +
     0.01 2014-08-09T08:51:55Z
    
         - original version

--- lib/App/GitGrepPerlStatement.pm
+++ lib/App/GitGrepPerlStatement.pm
@@ -5,7 +5,7 @@ use warnings;
 use App::GitGrepPerlStatement::StatementFinder;
 use Term::ANSIColor qw(colored);
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub run {
     my ($class, @argv) = @_;
